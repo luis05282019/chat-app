04841c3d0f8edbff44b16245c2e730f3e15a6692
luis <gariluis060908@gmail.com>
2024-09-20 12:49:49 +0800

    first commit

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import api from '../configs/api'
+import { useNavigate } from 'react-router-dom'
+
+const UserContext = createContext()
+
+const UserProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [token, setToken] = useState('')
+    const nav = useNavigate();
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        setToken('')
+        nav('/login')
+    }
+
+    useEffect(() => {
+        api.get('/currentUser', { headers: { Authorization: `Bearer ${token}` } })
+            .then(res => setUser(res.data.user))
+            .catch(() => {
+                localStorage.removeItem('token')
+                nav('/login')
+            })
+    }, [token])
+
+    return <UserContext.Provider value={{ user, setToken, logout }}>
+        {children}
+    </UserContext.Provider>
+}
+
+export default UserProvider
+export const useUser = () => useContext(UserContext)
---
+++ src/pages/Chats.jsx
@@ -0,0 +1,37 @@
+import { Box } from '@mui/material'
+import { blue } from '@mui/material/colors'
+import React, { useEffect, useState } from 'react'
+import ChatArea from '../components/chat/ChatArea'
+import ListOfUsers from '../components/chat/ListOfUsers'
+import Welcome from '../components/chat/Welcome'
+import api from '../configs/api'
+import { useUser } from '../contexts/UserContext'
+
+const Chats = () => {
+
+    const { user } = useUser()
+    const [selectedUser, setSelectedUser] = useState(null)
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        api.get('/getAllUsers')
+            .then(res => setUsers(res.data.users))
+    }, [])
+
+    return (
+        <Box sx={{ background: blue[100], height: '80vh', mt: 5, width: '80%', mx: 'auto', display: 'flex' }}>
+            {/* list of users */}
+            <ListOfUsers users={users} setSelectedUser={setSelectedUser} />
+
+            {
+                selectedUser ?
+                    <ChatArea selectedUser={selectedUser} /> :
+                    <Welcome user={user} />
+            }
+
+        </Box>
+    )
+}
+
+export default Chats
